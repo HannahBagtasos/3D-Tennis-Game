be13388266f5386813f2d3666d36dc52fb3d4fd5
Fola Ojewale <folaojewale@gmail.com>
2022-10-06 10:53:01 +0100

    TargetAim (in progress)

--- 3D Tennis/Assets/Player.cs
+++ 3D Tennis/Assets/Player.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float speed = 3f;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public Transform TargetAim
+    float speed = 3f;
+    bool hitting;
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Vertical");
-        float v = Input.GetAxisRaw("Horizontal");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            hitting = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.F))
+        {
+            hitting = false;
+        }
 
         if(h!=0||v!=0)
         {
-            transform.Translate(new Vector3(h,0,v)* speed* Time.deltaTime);
+            transform.Translate(new Vector3(v ,0 ,h )* speed* Time.deltaTime);
         }
     }
 }
