cceddf4e5ab7fdd449a723e686161eca3cd5aa04
Fola Ojewale <folaojewale@gmail.com>
2022-10-06 17:14:16 +0100

    TargetAim

--- 3D Tennis/Assets/Player.cs
+++ 3D Tennis/Assets/Player.cs
@@ -13,21 +13,12 @@ public class Player : MonoBehaviour
 
     public Transform TargetAim
     float speed = 3f;
-    bool hitting;
+    
     // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            hitting = true;
-        }
-        else if(Input.GetKeyUp(KeyCode.F))
-        {
-            hitting = false;
-        }
 
         if(h!=0||v!=0)
         {
